6c8b934bd247d941ffb6c0f0afa1222e80e9a3b8
demetgas <demet_gas@hotmail.com>
2024-02-07 19:37:58 +0100

    imported feature to home page

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,9 @@
 import "./featured.scss";
 
 export default function Featured() {
-  return <div>Featured</div>;
+  return (
+    <div className="featured">
+        <img src="https://www.allkpop.com/upload/2022/06/content/051737/1654465030-a2b874f2-0fa1-49d8-8e86-b690bdf98a3e.jpeg" alt="" />
+    </div>
+  );
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,16 +1,14 @@
+import Featured from "../components/featured/Featured";
 import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">
-    <Navbar/>
-    <img
-    width="100%"
-            className="img1"
-            src="https://www.allkpop.com/upload/2022/06/content/051737/1654465030-a2b874f2-0fa1-49d8-8e86-b690bdf98a3e.jpeg"
-            alt=""
-          />
-  </div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
