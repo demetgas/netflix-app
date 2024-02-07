19dbf8163b3a6dd5c1bc34f4af5cbe483f1cf55f
demetgas <demet_gas@hotmail.com>
2024-02-07 19:01:59 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,7 +24,13 @@ const Navbar = () => {
             src="https://www.allkpop.com/upload/2022/06/content/051737/1654465030-a2b874f2-0fa1-49d8-8e86-b690bdf98a3e.jpeg"
             alt=""
           />
-          <ArrowDropDown className="icon" />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Log Out</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
