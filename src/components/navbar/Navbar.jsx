87cd6d7c34ddc849633887d19e762012bdc3bd80
demetgas <demet_gas@hotmail.com>
2024-02-07 18:05:01 +0100

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import "./navbar.scss";
-
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="left"></div>
+        <div className="left">
+          <img className="img1"
+            src="https://upload.wikimedia.org/wikipedia/commons/3/30/StaryKIDSLOGO.png"
+            alt=""
+          />
+          <span>Home</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
         <div className="right"></div>
       </div>
     </div>
