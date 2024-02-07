a46d62506d49d1bc58368d362d4b3e6ec1d96a24
demetgas <demet_gas@hotmail.com>
2024-02-07 18:38:15 +0100

    img added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,6 +20,12 @@ const Navbar = () => {
           <Search />
           <span>KID</span>
           <Notifications />
+          <img
+            className="img2"
+            src="https://www.allkpop.com/upload/2022/06/content/051737/1654465030-a2b874f2-0fa1-49d8-8e86-b690bdf98a3e.jpeg"
+            alt=""
+          />
+          <ArrowDropDown />
         </div>
       </div>
     </div>
