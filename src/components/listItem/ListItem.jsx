e29cfe6cc8d242f3724456d900eccd24dca54406
demetgas <demet_gas@hotmail.com>
2024-02-08 20:27:38 +0100

    pics

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,5 +1,7 @@
 import "./listItem.scss";
 
 export default function ListItem() {
-  return <div className="listItem">ListItem</div>;
+  return <div className="listItem">
+    <img src="https://i1.sndcdn.com/artworks-sH9jc1uxQUfOQKUF-wsqYqA-t500x500.jpg" alt="" />
+  </div>;
 }
