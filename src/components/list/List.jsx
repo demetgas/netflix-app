1ab083d3db451286ae233af31672152e5ca12e14
demetgas <demet_gas@hotmail.com>
2024-02-08 19:43:36 +0100

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -3,15 +3,23 @@ import {
   ArrowForwardIosOutlined,
 } from "@material-ui/icons";
 import "./list.scss";
-import React from "react";
+import React, { useRef } from "react";
 import ListItem from "../listItem/ListItem";
 
 export default function List() {
+  const listRef = useRef();
+  const handleClick = (direction) => {
+    if (direction === "left") {
+    }
+  };
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="arrows left" />
+        <ArrowBackIosOutlined
+          onClick={() => handleClick("left")}
+          className="arrows left"
+        />
         <div className="container">
           <ListItem />
           <ListItem />
@@ -27,7 +35,10 @@ export default function List() {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className="arrows right"/>
+        <ArrowForwardIosOutlined
+          onClick={() => handleClick("right")}
+          className="arrows right"
+        />
       </div>
     </div>
   );
