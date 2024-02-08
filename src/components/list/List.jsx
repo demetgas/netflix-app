a69607146a429978a78986d9702b6f861c1d9bc6
demetgas <demet_gas@hotmail.com>
2024-02-08 19:53:30 +0100

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,18 +2,26 @@ import {
   ArrowBackIosOutlined,
   ArrowForwardIosOutlined,
 } from "@material-ui/icons";
-import "./list.scss";
-import React, { useRef } from "react";
+import { useRef, useState } from "react";
 import ListItem from "../listItem/ListItem";
+import "./list.scss";
 
 export default function List() {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
   const listRef = useRef();
-  let distance = listRef.current.getBoundingClientReact().x - 50;
+
   const handleClick = (direction) => {
-    if (direction === "left") {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
-    } else {
-      listRef.current.style.transform = `translateX(-230px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
   };
   return (
