dd20161807be9c2357b1ebeeb5cceb4dc8a0c7c5
demetgas <demet_gas@hotmail.com>
2024-02-08 20:31:05 +0100

    added icons

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,7 +1,26 @@
 import "./listItem.scss";
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
 
 export default function ListItem() {
-  return <div className="listItem">
-    <img src="https://i1.sndcdn.com/artworks-sH9jc1uxQUfOQKUF-wsqYqA-t500x500.jpg" alt="" />
-  </div>;
+  return (
+    <div className="listItem">
+      <img
+        src="https://i1.sndcdn.com/artworks-sH9jc1uxQUfOQKUF-wsqYqA-t500x500.jpg"
+        alt=""
+      />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownAltOutlined />
+        </div>
+      </div>
+    </div>
+  );
 }
