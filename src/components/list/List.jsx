714df173a2c0c9493396dd31a3794357de2a3dc2
demetgas <demet_gas@hotmail.com>
2024-02-08 19:05:36 +0100

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,10 +1,19 @@
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
 import "./list.scss";
 import React from "react";
 
 export default function List() {
   return (
     <div className="list">
-      <div></div>
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined />
+        <div className="container"></div>
+        <ArrowForwardIosOutlined />
+      </div>
     </div>
   );
 }
