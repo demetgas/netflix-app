c8b38cf77e82498e42f306c408c69de9de060cc9
demetgas <demet_gas@hotmail.com>
2024-02-09 14:13:36 +0100

    Update ListItem.jsx

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -9,35 +9,42 @@ import {
 
 export default function ListItem(index) {
   const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
     <div
       className="listItem"
-      style={{ left: isHovered && index * 225 - 50  + index * 2.5}}
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <img
-        src="https://upload.wikimedia.org/wikipedia/en/0/04/The_Addams_Family.jpg"
-        alt=""
-      />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
-        </div>
-        <div className="itemInfoTop">
-          <span>1 hour 34 mins</span>
-          <span>+9</span>
-          <span>1993</span>
-        </div>
-        <div className="desc">
-          The Addams Family try to rescue their beloved Uncle Fester from his
-          gold-digging new love, a black widow named Debbie.
-        </div>
-        <div className="genre">Action,Fantasy,Dark Comedy,Horror</div>
-      </div>
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/0/04/The_Addams_Family.jpg"
+            alt=""
+          />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow />
+              <Add />
+              <ThumbUpAltOutlined />
+              <ThumbDownAltOutlined />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 34 mins</span>
+              <span>+9</span>
+              <span>1993</span>
+            </div>
+            <div className="desc">
+              The Addams Family try to rescue their beloved Uncle Fester from
+              his gold-digging new love, a black widow named Debbie.
+            </div>
+            <div className="genre">Action,Fantasy,Dark Comedy,Horror</div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
