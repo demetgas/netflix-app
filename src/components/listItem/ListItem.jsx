648c8481bbe2fbb615eb529f09149012c09946cb
demetgas <demet_gas@hotmail.com>
2024-02-09 14:06:35 +0100

    Update ListItem.jsx

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -12,7 +12,7 @@ export default function ListItem(index) {
   return (
     <div
       className="listItem"
-      style={{ left: isHovered && index * 225 - 50 }}
+      style={{ left: isHovered && index * 225 - 50  + index * 2.5}}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
