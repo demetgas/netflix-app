0b8064b3cc9f522e0e975b7cd0c6d4d6dab05938
demetgas <demet_gas@hotmail.com>
2024-02-09 15:27:57 +0100

    Update Watch.jsx

--- src/watch/Watch.jsx
+++ src/watch/Watch.jsx
@@ -1,5 +1,10 @@
 import "./watch.scss";
+import {ArrowBackOutlined} from "@material-ui/icons"
 
 export default function Watch() {
-  return <div>Watch</div>;
+  return <div className="watch">
+    <div className="back">
+        <ArrowBackOutlined/>
+    </div>
+  </div>;
 }
